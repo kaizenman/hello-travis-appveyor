709917f3418cd1864f792bec7db98753a3459f70
Nikolay Lysenko <makolyan2@ukr.net>
2018-04-19 14:51:55 +0300

    added utils

---
+++ utils/Linux/get_latest_binary.sh
@@ -0,0 +1,6 @@
+curl -s https://api.github.com/repos/makolyan/hello-world/releases/latest \
+| grep "run_linux" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+chmod +x run_linux
---
+++ utils/Windows/get_latest_binary.ps1
@@ -0,0 +1,17 @@
+[Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12
+
+# Download latest dotnet/codeformatter release from github
+$repo = "makolyan/hello-world"
+$file = "run_windows.exe"
+
+$releases = "https://api.github.com/repos/$repo/releases"
+
+Write-Host Determining latest release
+$tag = (Invoke-WebRequest $releases -UseBasicParsing | ConvertFrom-Json)[0].tag_name
+
+$download = "https://github.com/$repo/releases/download/$tag/$file"
+$name = $file.Split(".")[0]
+$exe = "$name.exe"
+
+Write-Host Dowloading latest release
+Invoke-WebRequest $download -Out $exe -UseBasicParsing
