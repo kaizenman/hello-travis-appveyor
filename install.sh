78193c1a52a9d11e2c90ab5364346dcd01f14592
Nikolay <makolyan@icloud.com>
2018-04-21 22:02:47 +0300

    fix with bin directory (mac, linux)

--- install.sh
+++ install.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
-mkdir build
+if [ ! -d "build" ]; then
+   mkdir build
+fi
 cd build
 cmake ..
 make
 cd ..
+if [ ! -d "bin" ]; then
+   mkdir bin
+fi
 cp -vf build/bin/run_darwin bin/run_darwin
 rm -r build
