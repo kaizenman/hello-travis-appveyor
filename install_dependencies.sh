fc64f4092aa5a2021cf7e74c2880f3dc51a75b3a
Nikolay Lysenko <makolyan2@ukr.net>
2018-04-22 01:19:17 +0300

    wwww

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -11,20 +11,7 @@ if [[ "$TRAVIS_OS_NAME" == "osx" ]];
   cd "$currentDir"
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]];
   then echo "Installing for Linux"
-  sudo apt-get install \
-    xorg-dev \
-    mesa-utils \
-    libx11-dev \
-    libxrandr-dev \
-    libxinerama1-dev \
-    libxi-dev \
-    libXxf86vm-dev \
-    libXcursor-dev \
-    libGL-dev \
-    libgl1-mesa-dev \
-    libglu1-mesa-dev
-
-
+  sudo apt-get install xorg-dev mesa-utils libx11-dev libxrandr-dev libxinerama1-dev libxi-dev libXxf86vm-dev libXcursor-dev libGL-dev libgl1-mesa-dev libglu1-mesa-dev
   git clone https://github.com/glfw/glfw \
     && cd glfw \
     && mkdir build \
