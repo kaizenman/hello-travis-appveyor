1b7f77bbe835e4a5d8502f3a7d2101246cd73eb0
Nikolay <makolyan@icloud.com>
2018-04-21 22:37:36 +0300

    Fixed deploy for Linux

--- updater.sh
+++ updater.sh
@@ -1,11 +1,27 @@
 echo "Running updater..."
-if [ ! -d "bin" ]; then
+
+if [ ! -d ./.latest ]; then
+   mkdir ./.latest
+fi
+cd ./.latest
+if [ ! -d bin ]; then
    mkdir bin
 fi
 cd bin
+
+filename='unknown'
+
+if [ "$(uname)" == "Darwin" ]; then
+    filename='run_darwin'
+elif [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    filename='run_linux'
+fi
+
+
 curl -s https://api.github.com/repos/makolyan/hello-world/releases/latest \
-| grep "run_linux" \
+| grep "$filename*" \
 | cut -d : -f 2,3 \
 | tr -d \" \
-| wget -qi -N -
-chmod +x run_linux
+| wget -qi -
+
+chmod +x "$filename"
