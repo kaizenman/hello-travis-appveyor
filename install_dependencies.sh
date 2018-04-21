44e92b9a14549d0a6de05380cb8a76b7ef53edb7
Nikolay <makolyan@icloud.com>
2018-04-21 22:59:25 +0300

    one more fix....

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -8,17 +8,20 @@ currentDir=`pwd`
 if [[ "$TRAVIS_OS_NAME" == "osx" ]];
   then echo "Installing for osx"
   brew install glfw;
+  cd "$currentDir"
 elif [[ "$TRAVIS_OS_NAME" == "linux" ]];
   then echo "Installing for Linux"
   sudo apt-get install xorg-dev libglu1-mesa-dev
- 
+
   git clone https://github.com/glfw/glfw \
     && mkdir build \
     && cd build \
     && cmake ../glfw && make -j4 \
     && sudo make install
+
+  cd "$currentDir"
+  sudo rm -r glfw
 fi
-cd $currentDir
 
 #--------- GET GLEW ----------------------------------------------------------------------------------
 #-------------------------------------------------------------------------------------------------------------------
